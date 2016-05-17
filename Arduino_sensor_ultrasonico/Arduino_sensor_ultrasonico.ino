1b087e6bfca1e8657ba4cfdf3ffec473d505d62e
FenchDanilo <josedrc96@hotmail.com>
2016-05-16 21:14:01 -0600

    Add files via upload

---
+++ Arduino_sensor_ultrasonico/Arduino_sensor_ultrasonico.ino
@@ -0,0 +1,22 @@
+long distancia;
+long tiempo;
+void setup(){
+  Serial.begin(9600);
+  pinMode(9, OUTPUT );/*activacion del pin 9 como salida8*/
+  pinMode(8, INPUT); /*activacion del pin 8 como entrada8*/
+}
+
+void loop (){
+  digitalWrite(9,LOW);/*Por cuestion de estabilizacion del sensor*/
+  delayMicroseconds(5);
+  digitalWrite(9,HIGH);/* envio del pulso ultrasonico*/
+  delayMicroseconds(10);
+ tiempo=pulseIn(8,HIGH);/*Funcion para pedir la longitud del pulso entrante del pulso
+ultrasonico y cuando el sensor recibe el rebote, es decir: desde que deja de hacerlo, LOW,
+la longitud del pulso entrante*/
+distancia=int(0.017*tiempo);
+Serial.println ("Distancia");
+Serial.println(distancia);
+Serial.println("cm");
+delay(1000);
+}
---
+++ Pulsador/Pulsador.ino
@@ -0,0 +1,29 @@
+const int inputPin = 2;
+const int ledPin = 12;
+long randNumber;
+int value = 0;
+
+void setup () {
+  Serial.begin (9600);
+  pinMode (inputPin, INPUT );
+  pinMode(ledPin,OUTPUT);
+  randomSeed(analogRead(0));
+}
+
+void loop() {
+  value = digitalRead (inputPin); //lectura digital de pin
+  //Serial.println(value);
+  // mandar mensaje a puerto serie en funcion del valor leido
+   if (value ==1){
+      Serial.println("Encendido  Numero generado es : ");
+      randNumber= random(1, 50);
+      digitalWrite(ledPin, HIGH);
+      Serial.println (randNumber);
+   }
+   else {
+     Serial.println ("apagado");
+     //digitalWrite(ledPin,LOW);
+   }
+   delay (1);
+   //digitalWrite(ledPin,LOW);
+}
